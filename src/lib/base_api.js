f3ec0f3f5333daf5fb73dbe91651fc4f3066c339
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2025-01-13 01:30:04 +0300

    chore : switched to the new live server link

--- src/lib/base_api.js
+++ src/lib/base_api.js
@@ -4,7 +4,8 @@ import { devMode } from "./dev_mode";
 export const base_api = axios.create({
   baseURL: devMode
     ? "http://localhost:6969"
-    : "https://sancode-api.onrender.com",
+    : // : "https://sancode-api.onrender.com", -> OLD LIVE SERVER LINK
+      "https://san-code-api.lomogan.africa",
   headers: {
     "Content-Type": "application/json",
   },
