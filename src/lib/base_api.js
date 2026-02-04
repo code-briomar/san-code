614441ca771a66eff10c6623a917e5eb20b1ae83
briomar.ftw <kapolonbraine@gmail.com>
2026-02-04 16:38:33 +0300

    chore: changed base API ( using IP before domain name propagation )

--- src/lib/base_api.js
+++ src/lib/base_api.js
@@ -2,10 +2,11 @@ import axios from "axios";
 import { devMode } from "./dev_mode";
 
 export const base_api = axios.create({
-  baseURL: devMode
-    ? "http://localhost:5000"
-    : "https://sancode-api.onrender.com", // -> OLD LIVE SERVER LINK
+  // baseURL: devMode
+  //   ? "http://localhost:5000"
+  //   : "https://sancode-api.onrender.com", // -> OLD LIVE SERVER LINK
   // "https://san-code-api.lomogan.africa",
+  baseURL: "https://156.232.88.204/sanCode-API/",
   headers: {
     "Content-Type": "application/json",
   },
