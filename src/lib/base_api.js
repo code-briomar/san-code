9995f8dccf68e731db010ac652b930b50affe67e
briomar.ftw <kapolonbraine@gmail.com>
2026-02-25 19:12:06 +0300

    fix : "changed API provider"

--- src/lib/base_api.js
+++ src/lib/base_api.js
@@ -6,7 +6,7 @@ export const base_api = axios.create({
     // baseURL: "http://localhost:5000",
   //   : "https://sancode-api.onrender.com", // -> OLD LIVE SERVER LINK
   // "https://san-code-api.lomogan.africa",
-  baseURL: "https://lomogan.africa/sanCode-API/"|| "https://sancode-api.onrender.com",
+  baseURL: "https://sancode-api.onrender.com" ||"https://lomogan.africa/sanCode-API/"|| "https://sancode-api.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
